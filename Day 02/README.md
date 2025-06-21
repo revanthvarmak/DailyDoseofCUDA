94a263f0799e45cb84387c286b43de4459740634
revanthvarmak <revanthvarma.kantheti@gmail.com>
2025-06-20 21:40:44 -0400

    Day 02 learnings

---
+++ Day 02/README.md
@@ -0,0 +1,4 @@
+# Day 2: Vector Addition
+
+## Key Learnings
+I learned how to write and launch a simple CUDA kernel for vector addition. I understood the syntax and different ways to allocate host memory (pinned and pageable) and GPU device memory. Also learned launching a kernel using proper grid and block sizes
---
+++ Day 02/vectorAdd.cu
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cuda_runtime.h>
+
+
+
+__global__ void addVectors(float* dA, float* dB, float* dC, int N){
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    if(tid < N){
+        dC[tid] = dA[tid] + dB[tid];
+    }
+}
+
+
+int main(){
+    int N = 1 << 20;
+    size_t bytes = N * sizeof(float);
+    float *hA, *hB, *hC, *dA, *dB, *dC;
+
+    // This allocates pinned memory on the Host side, which is non-pageable and faster
+    // For allocating pageable memory, use malloc
+    cudaMallocHost(&hA, bytes);
+    cudaMallocHost(&hB, bytes);
+    cudaMallocHost(&hC, bytes);
+
+    for(int i = 0; i < N; ++i){
+        hA[i] = i;
+        hB[i] = i + 7;
+    }
+
+    // This allocates memory on the devide/CUDA 
+    cudaMalloc(&dA, bytes);
+    cudaMalloc(&dB, bytes);
+    cudaMalloc(&dC, bytes);
+
+    cudaMemcpy(dA, hA, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(dB, hB, bytes, cudaMemcpyHostToDevice);
+
+    int blockSize = 256;
+    int gridSize = (blockSize + N - 1) / blockSize;
+
+    addVectors<<<gridSize, blockSize>>>(dA, dB, dC, N);
+
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(hC, dC, bytes, cudaMemcpyDeviceToHost);
+
+    for(int i = 0; i < 10; ++i){
+        std::cout << hC[i] << std::endl;
+    }
+
+    cudaFree(dA);
+    cudaFree(dB); 
+    cudaFree(dC);
+
+    cudaFreeHost(hA);
+    cudaFreeHost(hB);
+    cudaFreeHost(hC);
+
+}
